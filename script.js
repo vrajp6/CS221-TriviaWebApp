a880b2f921f4bf383e741ce45f71a9ab9c416578
vrajp6 <vrajpatel2021@gmail.com>
2023-11-30 00:50:31 -0600

    questions and style update

--- script.js
+++ script.js
@@ -23,6 +23,39 @@ let questions = [
   },
 
   {
+    category: 'sports',
+    question: 'Who won the 2022 FIFA World Cup ?',
+    answers: [
+      { text: 'Brazil', correct: false },
+      { text: 'France', correct: false },
+      { text: 'Argentina', correct: true },
+      { text: 'Italy', correct: false },
+    ],
+  },
+
+  {
+    category: 'sports',
+    question: 'Which female tennis player won the most Grand Slam singles titles in the 2010s ?',
+    answers: [
+      { text: 'Serena Williams', correct: true },
+      { text: 'Maria Sharapova', correct: false },
+      { text: 'Simona Halep', correct: false },
+      { text: 'Naomi Osaka', correct: false },
+    ],
+  },
+
+  {
+    category: 'sports',
+    question: 'Which team won the Super Bowl in 2021 ?',
+    answers: [
+      { text: 'New England Patriots', correct: false },
+      { text: 'Tampa Bay Buccaneers', correct: true },
+      { text: 'Kansas City Chiefs', correct: false },
+      { text: 'San Francisco 49ers', correct: false },
+    ],
+  },
+
+  {
     category: 'science',
     question: 'How many elements are listed in the periodic table ?',
     answers: [
@@ -30,6 +63,50 @@ let questions = [
       { text: '102', correct: false },
       { text: '64', correct: false },
       { text: '118', correct: true },
+    ],
+  },
+
+  {
+    category: 'science',
+    question: 'Who developed the theory of general relativity ?',
+    answers: [
+      { text: 'Isaac Newton', correct: false },
+      { text: 'Albert Einstein', correct: true },
+      { text: 'Nikola Tesla', correct: false },
+      { text: 'Stephen Hawking', correct: false },
+    ],
+  },
+
+  {
+    category: 'science',
+    question: 'What type of animal is a dolphin ?',
+    answers: [
+      { text: 'Fish', correct: false },
+      { text: 'Amphibian', correct: false },
+      { text: 'Reptile', correct: false },
+      { text: 'Mammal', correct: true },
+    ],
+  },
+
+  {
+    category: 'science',
+    question: "What is the most abundant gas in the Earth's atmosphere ?",
+    answers: [
+      { text: 'Oxygen', correct: false },
+      { text: 'Carbon Dioxide', correct: false },
+      { text: 'Nitrogen', correct: true },
+      { text: 'Hydrogen', correct: false },
+    ],
+  },
+
+  {
+    category: 'science',
+    question: 'What is the chemical symbol for gold ?',
+    answers: [
+      { text: 'Au', correct: true },
+      { text: 'Ag', correct: false },
+      { text: 'Go', correct: false },
+      { text: 'Gd', correct: false },
     ],
   },
 
@@ -46,6 +123,54 @@ let questions = [
   },
 
   {
+    category: 'music',
+    question:
+      'Which artist is known for the hit song "Shape of You" ?',
+    answers: [
+      { text: 'Ed Sheeran', correct: true },
+      { text: 'Justin Bieber', correct: false },
+      { text: 'Shawn Mendes', correct: false },
+      { text: 'Harry Styles', correct: false },
+    ],
+  },
+
+  {
+    category: 'music',
+    question:
+      'Which band wrote and performed "Bohemian Rhapsody" ?',
+    answers: [
+      { text: 'The Beatles', correct: false },
+      { text: 'Pink Floyd', correct: false },
+      { text: 'Queen', correct: true },
+      { text: 'Led Zeppelin', correct: false },
+    ],
+  },
+
+  {
+    category: 'music',
+    question:
+      'What genre of music did artists like Duke Ellington and Louis Armstrong popularize ?',
+    answers: [
+      { text: 'Rock', correct: false },
+      { text: 'Jazz', correct: true },
+      { text: 'Blues', correct: false },
+      { text: 'Pop', correct: false },
+    ],
+  },
+
+  {
+    category: 'music',
+    question:
+      'Who is known for the hit single "Old Town Road" ?',
+    answers: [
+      { text: 'Drake', correct: false },
+      { text: 'Travis Scott', correct: false },
+      { text: 'Lil Nas X', correct: true },
+      { text: 'Post Malone', correct: false },
+    ],
+  },
+
+  {
     category: 'history',
     question: 'The United States bought Alaska from which country ?',
     answers: [
@@ -53,6 +178,50 @@ let questions = [
       { text: 'Japan', correct: false },
       { text: 'Russia', correct: true },
       { text: 'Africa', correct: false },
+    ],
+  },
+
+  {
+    category: 'history',
+    question: 'Which empire was ruled by emperors such as Julius Caesar and Augustus ?',
+    answers: [
+      { text: 'British Empire', correct: false },
+      { text: 'Ottoman Empire', correct: false },
+      { text: 'Roman Empire', correct: true },
+      { text: 'Mongol Empire', correct: false },
+    ],
+  },
+
+  {
+    category: 'history',
+    question: 'In what year did the Titanic sink ?',
+    answers: [
+      { text: '1905', correct: false },
+      { text: '1912', correct: true },
+      { text: '1920', correct: false },
+      { text: '1931', correct: false },
+    ],
+  },
+
+  {
+    category: 'history',
+    question: 'What event began on July 28, 1914, and lasted until November 11, 1918 ?',
+    answers: [
+      { text: 'The American Civil War', correct: false },
+      { text: 'The Russian Revolution', correct: false },
+      { text: 'The Great Depression', correct: false },
+      { text: 'World War I', correct: true },
+    ],
+  },
+
+  {
+    category: 'history',
+    question: 'What was the main reason for the construction of the Berlin Wall ?',
+    answers: [
+      { text: 'To protect against invasions', correct: false },
+      { text: 'To prevent the spread of communism', correct: false },
+      { text: 'To separate East and West Berlin', correct: true },
+      { text: 'To create a tourist attraction', correct: false },
     ],
   },
 ];
@@ -131,6 +300,7 @@ function selectAnswer(e) {
 
     if (correct) {
         selectedButton.classList.add('btn-correct');
+        score++;
     } else {
         selectedButton.classList.add('btn-incorrect');
     }
