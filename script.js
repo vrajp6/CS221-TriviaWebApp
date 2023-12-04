222cfacc5152607e88173bb64bbf992943eca316
coolier <collier.mingus@gmail.com>
2023-12-04 14:22:35 -0600

    sound fix

--- script.js
+++ script.js
@@ -395,11 +395,11 @@ function triggerConfetti() {
   });
 }
 // Function to play category music
-function playCategoryMusic() {
-  var categoryMusic = document.getElementById('category-music');
-  categoryMusic.volume = 0.1; // Set the volume to 10%
-  categoryMusic.play();
-}
+// function playCategoryMusic() {
+//   var categoryMusic = document.getElementById('category-music');
+//   categoryMusic.volume = 0.1; // Set the volume to 10%
+//   categoryMusic.play();
+// }
 
 // Function to stop category music and play question music
 function playQuestionMusic() {
